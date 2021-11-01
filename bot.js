f66c760cda658f218effcb77cf7cfd88c241de1f
Manuel <merodriguez9112@gmail.com>
2021-11-01 08:02:58 -0400

    testing

--- bot.js
+++ bot.js
@@ -357,9 +357,10 @@ function generate_message(arr) {
 
 // -------------cron job ---------------------------
 
-cron.schedule('0 11 * * *', () => {
+// cron.schedule('0 11 * * *', () => {
     craw.queue(qvalive_url);
-});
+console.log(moment().format('DDMMYYYY'));
+// });
 
 // -------------bot - handler ---------------------------
 
